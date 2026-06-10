fcedfd4d11ef26ea8a94e8d371dc02c1014af3e6
Тимур <tima.gorenskiy@mail.ru>
2026-06-10 18:58:47 +0300

    cosmetics

--- internal/dto/update-experement.go
+++ internal/dto/update-experement.go
@@ -3,8 +3,7 @@ package dto
 import "time"
 
 type UpdateExperiment struct {
-	ID int64 `json:"id"`
-
+	ID                int64      `json:"id"`
 	Name              *string    `json:"name"`
 	RolloutPercentage *int64     `json:"rollout_percentage"`
 	Bucket            []int64    `json:"bucket"`
